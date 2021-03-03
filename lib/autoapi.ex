38d69c653412f48e8167bb7ace2230cb16d4bc57
lucascharlesz <lucascharlesz@hotmail.com>
2021-03-02 23:49:31 -0300

    project created

---
+++ lib/autoapi.ex
@@ -0,0 +1,18 @@
+defmodule Autoapi do
+  @moduledoc """
+  Documentation for `Autoapi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Autoapi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/autoapi_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoapiTest do
+  use ExUnit.Case
+  doctest Autoapi
+
+  test "greets the world" do
+    assert Autoapi.hello() == :world
+  end
+end
